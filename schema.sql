932a3b55125aed34dfd157712614d9da13e319b9
emoodyv <colliermoody5@gmail.com>
2020-11-05 21:19:53 -0700

    Pewlett Hackard Analysis

--- schema.sql
+++ schema.sql
@@ -1,4 +1,23 @@
 -- Creating tables for PH-EmployeeDB
+CREATE TABLE dept_emp (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, title),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
