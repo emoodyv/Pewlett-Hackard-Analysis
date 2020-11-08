1730e278fba313118e6b900cfcbf8edb19c2aa8a
emoodyv <colliermoody5@gmail.com>
2020-11-08 14:51:55 -0700

    Pewlett Hackard Analysis

---
+++ Employee_Challenge.sql
@@ -0,0 +1,23 @@
+-- Getting Data
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+ORDER BY _____, _____ DESC;
