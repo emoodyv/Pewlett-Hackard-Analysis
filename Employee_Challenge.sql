d40b068d3b5a34dce88f4859505cf35d14ec4d21
emoodyv <colliermoody5@gmail.com>
2020-11-08 15:38:37 -0700

    Pewlett Hackard Analysis

--- Employee_Challenge.sql
+++ Employee_Challenge.sql
@@ -10,14 +10,13 @@ FROM employees as e
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY emp_no ASC;
+ORDER BY emp_no, title ASC;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
-
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no DESC;
